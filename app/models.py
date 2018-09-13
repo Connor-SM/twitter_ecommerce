a7204ce207fbaa1101a35dede10d92954e183d18
Connor Milliken <cmilliken09@gmail.com>
2018-09-13 08:03:21 -0400

    Added User Table

--- app/models.py
+++ app/models.py
@@ -2,6 +2,17 @@ from app import db
 from datetime import datetime
 
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    age = db.Column(db.Integer)
+    bio = db.Column(db.String(50))
+    url = db.Column(db.String(100))
+
+    def __repr__(self):
+        return 'User {} is {} years old. Bio - {}'.format(self.name, self.age, self.bio)
+
+
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     tweet = db.Column(db.String(140))
--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,7 @@ from app import app, db
 from flask import render_template, url_for, redirect, flash
 from app.forms import PostForm, TitleForm
 import datetime
-from app.models import Post
+from app.models import Post, User
 
 
 @app.route('/')
@@ -61,68 +61,69 @@ def index(title=''):
     }
     return render_template('index.html', products=products, title=title, page='home')
 
-posts_dict = {
-    0: {
-        'date': 'Sept. 9th, 2018',
-        'name': 'Max',
-        'tweet': 'Today I had cereal for breakfast.'
-    },
-    1: {
-        'date': 'July 1st, 2018',
-        'name': 'Kelly',
-        'tweet': 'Went for a run downtown.'
-    },
-    2: {
-        'date': 'June 21st, 2018',
-        'name': 'Max',
-        'tweet': 'Got a new job!! Working for the man.'
-    },
-    3: {
-        'date': 'March 4th, 2018',
-        'name': 'Kelly',
-        'tweet': 'Hiking is fun, get outside.'
-    },
-    4: {
-        'date': 'February 8th, 2018',
-        'name': 'Kelly',
-        'tweet': 'This is a sample text. This is a sample text.'
-    },
-    5: {
-        'date': 'October 10th, 2017',
-        'name': 'Max',
-        'tweet': 'This is a sample text. This is a sample text.'
-    },
-    6: {
-        'date': 'October 1st, 2017',
-        'name': 'Max',
-        'tweet': 'This is a sample text. This is a sample text.'
-    },
-    7: {
-        'date': 'Sept. 31st, 2017',
-        'name': 'Kelly',
-        'tweet': 'This is a sample text. This is a sample text.'
-    }
-}
+# posts_dict = {
+#     0: {
+#         'date': 'Sept. 9th, 2018',
+#         'name': 'Max',
+#         'tweet': 'Today I had cereal for breakfast.'
+#     },
+#     1: {
+#         'date': 'July 1st, 2018',
+#         'name': 'Kelly',
+#         'tweet': 'Went for a run downtown.'
+#     },
+#     2: {
+#         'date': 'June 21st, 2018',
+#         'name': 'Max',
+#         'tweet': 'Got a new job!! Working for the man.'
+#     },
+#     3: {
+#         'date': 'March 4th, 2018',
+#         'name': 'Kelly',
+#         'tweet': 'Hiking is fun, get outside.'
+#     },
+#     4: {
+#         'date': 'February 8th, 2018',
+#         'name': 'Kelly',
+#         'tweet': 'This is a sample text. This is a sample text.'
+#     },
+#     5: {
+#         'date': 'October 10th, 2017',
+#         'name': 'Max',
+#         'tweet': 'This is a sample text. This is a sample text.'
+#     },
+#     6: {
+#         'date': 'October 1st, 2017',
+#         'name': 'Max',
+#         'tweet': 'This is a sample text. This is a sample text.'
+#     },
+#     7: {
+#         'date': 'Sept. 31st, 2017',
+#         'name': 'Kelly',
+#         'tweet': 'This is a sample text. This is a sample text.'
+#     }
+# }
 
 @app.route('/posts/<name>', methods=['GET', 'POST'])
 def posts(name = 'Max'):
     form = PostForm()
 
-    people = {
-        0: {
-            'name': 'Max',
-            'age': 26,
-            'bio': 'Avid swimmer, cat lover, and I ride a bike everywhere.',
-            'url': 'http://placehold.it/250x250'
-        },
-        1: {
-            'name': 'Kelly',
-            'age': 21,
-            'bio': 'My name is Kelly, I work in Boston and love dogs <3.',
-            'url': 'http://placehold.it/250x250'
-        }
-    }
+    # people = {
+    #     0: {
+    #         'name': 'Max',
+    #         'age': 26,
+    #         'bio': 'Avid swimmer, cat lover, and I ride a bike everywhere.',
+    #         'url': 'http://placehold.it/250x250'
+    #     },
+    #     1: {
+    #         'name': 'Kelly',
+    #         'age': 21,
+    #         'bio': 'My name is Kelly, I work in Boston and love dogs <3.',
+    #         'url': 'http://placehold.it/250x250'
+    #     }
+    # }
 
+    people = User.query.all()
     posts = Post.query.all()
 
     if form.validate_on_submit():
