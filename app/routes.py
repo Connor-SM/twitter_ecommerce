4b0384dbeeef957375e97411b79bae8daa78fedd
Connor Milliken <cmilliken09@gmail.com>
2018-09-20 11:55:08 -0400

    Login Updated

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,9 @@
 from app import app, db
 from flask import render_template, url_for, redirect, flash
-from app.forms import PostForm, TitleForm
+from app.forms import PostForm, TitleForm, LoginForm, RegisterForm
 import datetime
-from app.models import Post
+from app.models import Post, User
+from flask_login import current_user, login_user, logout_user, login_required
 
 
 @app.route('/')
@@ -61,68 +62,70 @@ def index(title=''):
     }
     return render_template('index.html', products=products, title=title, page='home')
 
-posts_dict = {
-    0: {
-        'date': 'Sept. 9th, 2018',
-        'name': 'Max',
-        'tweet': 'Today I had cereal for breakfast.'
-    },
-    1: {
-        'date': 'July 1st, 2018',
-        'name': 'Kelly',
-        'tweet': 'Went for a run downtown.'
-    },
-    2: {
-        'date': 'June 21st, 2018',
-        'name': 'Max',
-        'tweet': 'Got a new job!! Working for the man.'
-    },
-    3: {
-        'date': 'March 4th, 2018',
-        'name': 'Kelly',
-        'tweet': 'Hiking is fun, get outside.'
-    },
-    4: {
-        'date': 'February 8th, 2018',
-        'name': 'Kelly',
-        'tweet': 'This is a sample text. This is a sample text.'
-    },
-    5: {
-        'date': 'October 10th, 2017',
-        'name': 'Max',
-        'tweet': 'This is a sample text. This is a sample text.'
-    },
-    6: {
-        'date': 'October 1st, 2017',
-        'name': 'Max',
-        'tweet': 'This is a sample text. This is a sample text.'
-    },
-    7: {
-        'date': 'Sept. 31st, 2017',
-        'name': 'Kelly',
-        'tweet': 'This is a sample text. This is a sample text.'
-    }
-}
+# posts_dict = {
+#     0: {
+#         'date': 'Sept. 9th, 2018',
+#         'name': 'Max',
+#         'tweet': 'Today I had cereal for breakfast.'
+#     },
+#     1: {
+#         'date': 'July 1st, 2018',
+#         'name': 'Kelly',
+#         'tweet': 'Went for a run downtown.'
+#     },
+#     2: {
+#         'date': 'June 21st, 2018',
+#         'name': 'Max',
+#         'tweet': 'Got a new job!! Working for the man.'
+#     },
+#     3: {
+#         'date': 'March 4th, 2018',
+#         'name': 'Kelly',
+#         'tweet': 'Hiking is fun, get outside.'
+#     },
+#     4: {
+#         'date': 'February 8th, 2018',
+#         'name': 'Kelly',
+#         'tweet': 'This is a sample text. This is a sample text.'
+#     },
+#     5: {
+#         'date': 'October 10th, 2017',
+#         'name': 'Max',
+#         'tweet': 'This is a sample text. This is a sample text.'
+#     },
+#     6: {
+#         'date': 'October 1st, 2017',
+#         'name': 'Max',
+#         'tweet': 'This is a sample text. This is a sample text.'
+#     },
+#     7: {
+#         'date': 'Sept. 31st, 2017',
+#         'name': 'Kelly',
+#         'tweet': 'This is a sample text. This is a sample text.'
+#     }
+# }
 
 @app.route('/posts/<name>', methods=['GET', 'POST'])
-def posts(name = 'Max'):
+@login_required
+def posts(name):
     form = PostForm()
 
-    people = {
-        0: {
-            'name': 'Max',
-            'age': 26,
-            'bio': 'Avid swimmer, cat lover, and I ride a bike everywhere.',
-            'url': 'http://placehold.it/250x250'
-        },
-        1: {
-            'name': 'Kelly',
-            'age': 21,
-            'bio': 'My name is Kelly, I work in Boston and love dogs <3.',
-            'url': 'http://placehold.it/250x250'
-        }
-    }
+    # people = {
+    #     0: {
+    #         'name': 'Max',
+    #         'age': 26,
+    #         'bio': 'Avid swimmer, cat lover, and I ride a bike everywhere.',
+    #         'url': 'http://placehold.it/250x250'
+    #     },
+    #     1: {
+    #         'name': 'Kelly',
+    #         'age': 21,
+    #         'bio': 'My name is Kelly, I work in Boston and love dogs <3.',
+    #         'url': 'http://placehold.it/250x250'
+    #     }
+    # }
 
+    people = User.query.all()
     posts = Post.query.all()
 
     if form.validate_on_submit():
@@ -144,6 +147,7 @@ def posts(name = 'Max'):
 
 
 @app.route('/title', methods=['GET', 'POST'])
+@login_required
 def title():
     form2 = TitleForm()
 
@@ -152,3 +156,46 @@ def title():
         return redirect(url_for('index', title=title))
 
     return render_template('title.html', form=form2, page='title')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Credentials are incorrect')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        flash('You are now logged in.')
+        return redirect(url_for('posts', name=user.name))
+
+    return render_template('login.html', form=form, page='login')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+
+    form = RegisterForm()
+
+    if form.validate_on_submit():
+        user = User(name=form.name.data, username=form.username.data, email=form.email.data, bio=form.bio.data, age=form.age.data, url=form.url.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you have successfully registered.')
+        return redirect(url_for('login'))
+
+    return render_template('register.html', form=form, page='register')
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
